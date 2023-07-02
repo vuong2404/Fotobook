e1a27e4150b8bba0fe2abe652dad8ed17d046eb2
vuong2404 <minhvuonglht@gmail.com>
2023-07-02 15:41:44 +0700

    add login, register page using devise

--- app/models/connection.rb
+++ app/models/connection.rb
@@ -1,2 +1,3 @@
 class Connection < ApplicationRecord
+    
 end
--- db/migrate/20230627073654_create_users.rb
+++ db/migrate/20230627073654_create_users.rb
@@ -3,7 +3,6 @@ class CreateUsers < ActiveRecord::Migration[7.0]
     create_table :users do |t|
       t.string :fname
       t.string :lname
-      t.string :email
       t.string :role
       t.string :password
       t.binary :avatar
