2c8f3884a316f3f434addcb8e2a6485db1fa9534
vuong2404 <minhvuonglht@gmail.com>
2023-07-26 22:40:17 +0700

    fix fb login

--- app/models/user.rb
+++ app/models/user.rb
@@ -50,26 +50,16 @@ class User < ApplicationRecord
 
   
   def self.from_omniauth(auth)
-    find_or_create_by(provider: auth.provider, uid: auth.uid) do |user|
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
-      user.name = auth.info.name   # assuming the user model has a name
-      user.image = auth.info.image # assuming the user model has an image
-      # If you are using confirmable and the provider(s) you use validate emails, 
-      # uncomment the line below to skip the confirmation emails.
-      # user.skip_confirmation!
+      user.fname = auth.info.name 
+      user.avatar.attach(io: URI.open(auth.info.image), filename: "avatar#{auth.uid}") 
     end
   end
 
-  def self.new_with_session(params, session)
-    super.tap do |user|
-      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-        user.email = data["email"] if user.email.blank?
-      end
-    end
-  end
+end
 
-  
 
   private
 
