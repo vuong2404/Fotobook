ae2a84f3fb61e1db348b3e4c2ddd3da4b0898ea7
vuong2404 <minhvuonglht@gmail.com>
2023-07-26 21:35:09 +0700

    try omniauth by facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   paginates_per 25
   devise :database_authenticatable, :registerable,
-        :recoverable, :rememberable, :validatable, :omniauthable, :omniauth_providers => [:google_oauth2]
+        :recoverable, :rememberable, :validatable, :omniauthable, :omniauth_providers => [:google_oauth2, :facebook]
 
   attr_readonly :role
   before_create :set_default_value
@@ -50,13 +50,26 @@ class User < ApplicationRecord
 
   
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    find_or_create_by(provider: auth.provider, uid: auth.uid) do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
-      user.fname = auth.info.name 
-      user.avatar.attach(io: URI.open(auth.info.image), filename: "avatar#{auth.uid}") 
+      user.name = auth.info.name   # assuming the user model has a name
+      user.image = auth.info.image # assuming the user model has an image
+      # If you are using confirmable and the provider(s) you use validate emails, 
+      # uncomment the line below to skip the confirmation emails.
+      # user.skip_confirmation!
     end
   end
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
+
+  
 
   private
 
